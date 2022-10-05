d2a6be9c0ca3b7916a481a9f10a235d87b209753
Jeremy Kaplan <jdkaplan@metagram.net>
2022-10-04 23:34:19 -0700

    Clean up some TODO comments

--- js/nav.ts
+++ js/nav.ts
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.querySelector("#nav-toggle");
 
   // Clear any old event handlers to make this idempotent. Without this, the
-  // menu toggle stops working because it immediatelly re-hides after showing.
+  // menu toggle stops working because it immediately re-hides after showing.
   //
   // TODO: Is this fixed now that Turbo(links) isn't involved?
   toggle.removeEventListener("click", toggleMenu);
