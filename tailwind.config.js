4303cc236583f0a3142ffce5d8ee65eec72bdeb9
Jeremy Kaplan <jdkaplan@metagram.net>
2022-10-06 21:20:52 -0700

    Show all licenses on a page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html"],
+  content: ["./templates/**/*.html", "./templates/**/*.hbs"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
