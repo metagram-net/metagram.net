b397a6d0dbf568b46cdcaa70c6096378534764f8
Jeremy Kaplan <jdkaplan@metagram.net>
2023-02-13 22:36:59 +0100

    Update to esbuild v0.17 context API

--- build.mjs
+++ build.mjs
@@ -1,33 +1,22 @@
 import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 
-let watch = null;
-if (process.argv[2] === "watch") {
-  watch = {
-    onRebuild(err) {
-      if (err) {
-        console.error("watch build failed:", err);
-      } else {
-        console.log("watch build succeeded");
-      }
-    },
-  };
-}
-
-esbuild
-  .build({
+const context = await esbuild.context({
     entryPoints: ["js/app.ts", "js/firehose.ts"],
     bundle: true,
     sourcemap: true,
     target: "es6",
     plugins: [sassPlugin()],
     outdir: "dist/js",
-    watch,
-  })
-  .then(() => {
-    console.log("esbuild succeeded");
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    logLevel: "info",
+});
+
+if (process.argv[2] === "watch") {
+    await context.watch();
+    // TODO: Is it better to serve here than from Rust now that it's possible?
+} else {
+    await context.rebuild();
+    console.log("esbuild finished");
+
+    await context.dispose();
+}
